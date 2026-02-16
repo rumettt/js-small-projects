f80c3814f439daf197d4482f87531894a19e6535
ruhamateshager1@gmail.com <ruhamateshager1@gmail.com>
2026-02-15 22:26:46 -0800

    add pig-game projects

---
+++ pig-game/starter/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const currentE0 = document.getElementById('.current--0');
+const currentE1 = document.getElementById('.current--1');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    currentE0.textContent = currentScore;
+  } else {
+  }
+});
